bd3ced5c2be3a8f487a9ff84a3385ca312e85b3e
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2023-01-01 15:04:59 +0330

    create text pairs english and spanish words

--- translation.py
+++ translation.py
@@ -1,7 +1,19 @@
 
 
+# Data and Preprocessing 
+# Load dataset
 path = "D:\Python_Codes\Deep_Learning\Term 2\Translation\spa.txt"
 def load_doc(path): # load dataset
     with open(path, encoding="utf-8") as f:
         lines = f.read().split("\n")[-1] # read data --> split them every line --> don't read the last line because it's empty
     return lines
+lines = load_doc(path)
+
+# Esp-Eng pairs
+def creat_pairs(lines): 
+    text_pairs = []
+    for line in lines:
+        english, spanish = line.split("\t") # creating english and spanish words by camma
+        spanish = "[start]" + spanish + "[end]" # Add [start] and [end] to spanish words
+        text_pairs.append((english, spanish))
+        return text_pairs
