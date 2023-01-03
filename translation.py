9fa39a4d2795ea4eb3d17416163a72795cbedfb1
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2023-01-03 08:19:19 +0330

    Split data to train, validation and test data

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
-
+import random
 
 # Data and Preprocessing 
 # Load dataset
@@ -19,3 +19,17 @@ def creat_pairs(lines):
         text_pairs.append((english, spanish))
         return text_pairs
 text_pairs = creat_pairs(lines)
+
+# Split data to train-test-validation
+train_per = 0.7
+test_per = 0.15
+val_per = 0.15
+def split_data(text_pairs):
+    random.shuffle(text_pairs) # shuffle text data
+    num_train_data = train_per * (len(text_pairs)) # number of train data
+    #num_test_data = test_per * (len(text_pairs)) # number of test data
+    num_val_data = val_per * (len(text_pairs)) # number of validation data
+    train_pairs = text_pairs[:num_train_data] # train data from 0 to the end of train data numbers
+    val_pairs = text_pairs[num_train_data:num_train_data + num_val_data] # validation data from train numbers to the sum of train and validation numbers
+    test_pairs = text_pairs[num_train_data + num_val_data:] # test data from sum of train and validation numbers to the end
+    return train_pairs, val_pairs, test_pairs
